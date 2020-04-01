d2b92f549d105ca6b1326486ec9de28f3d7be3fe
unknown <ayman.bmh123@gmail.com>
2020-04-01 18:53:34 +0100

    Updates

---
+++ precache-manifest.ced28c5f0991509045c2ed75b46fd6ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27097b6eee88d746ebd92d4d5e1b98d9",
+    "url": "/RoboticsFriends/index.html"
+  },
+  {
+    "revision": "258ee5beb9f5b1f43edb",
+    "url": "/RoboticsFriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "3d8a91ffa54926cab7ab",
+    "url": "/RoboticsFriends/static/css/main.3702052b.chunk.css"
+  },
+  {
+    "revision": "258ee5beb9f5b1f43edb",
+    "url": "/RoboticsFriends/static/js/2.865fb8d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboticsFriends/static/js/2.865fb8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8a91ffa54926cab7ab",
+    "url": "/RoboticsFriends/static/js/main.7d93dea5.chunk.js"
+  },
+  {
+    "revision": "8dc1de14f8585681f0dc",
+    "url": "/RoboticsFriends/static/js/runtime-main.1e8af0ad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboticsFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
